6029a730b5bb8ad88d540afbc39a8ff02fc0c647
Enis <enis.mema@gmail.com>
2023-05-11 17:48:19 +0200

    Add my_list.rb and my_enumerable

--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,27 @@
+# Create a class MyList that has an instance variable @list.
+
+require_relative 'my_enumerable'
+
 class MyList
   def initialize(*list)
     @list = list
   end
+
+include MyEnumerable
+
+  def each  
+    @list.each { |n| yield n if block_given? }
+  end
 end
+
+
+list = MyList.new(1, 2, 3, 4)
+
+puts(list.all? { |e| e < 5 }) #=> true
+puts(list.all? { |e| e > 5 }) #=> false
+
+
+puts(list.any? { |e| e == 2 }) #=> true
+puts(list.any? { |e| e == 5 }) #=> false
+
+puts list.filter {|e| e.even?} #=> [2, 4]
