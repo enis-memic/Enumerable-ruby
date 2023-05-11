8cc659a0a922c1d87b87307fd693a332aebef8ff
Enis <enis.mema@gmail.com>
2023-05-11 17:48:59 +0200

    Fix linters

--- my_list.rb
+++ my_list.rb
@@ -7,21 +7,19 @@ class MyList
     @list = list
   end
 
-include MyEnumerable
+  include MyEnumerable
 
-  def each  
+  def each
     @list.each { |n| yield n if block_given? }
   end
 end
-
 
 list = MyList.new(1, 2, 3, 4)
 
 puts(list.all? { |e| e < 5 }) #=> true
 puts(list.all? { |e| e > 5 }) #=> false
 
-
 puts(list.any? { |e| e == 2 }) #=> true
 puts(list.any? { |e| e == 5 }) #=> false
 
-puts list.filter {|e| e.even?} #=> [2, 4]
+puts list.filter(&:even?) #=> [2, 4]
